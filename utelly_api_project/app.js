c795a538693891f923b24fa74220a723e512494f
Deva <dkothand@gmail.com>
2019-04-05 13:51:09 -0400

    testing parseData function returns name, picture, first streaming name and streaming url

--- utelly_api_project/app.js
+++ utelly_api_project/app.js
@@ -8,7 +8,68 @@
 // https://stackoverflow.com/questions/19059580/client-on-node-uncaught-referenceerror-require-is-not-defined
 //============================================================================
 // test url :"https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=bojack&country=uk"
+
+
+// Current Task: 
+// Add html containers for fields
+// create input box and submit button
+// test populating containers with ONE result from search
+
+
+// API data
+    // data.results is array []
+    // each element in array is an object
+    // object properties:
+
+        //id: identifier for that movie/tv show
+        //locations: array [] of objects {}
+            // each object is a streaming option
+            // object properties:
+
+                //display_name: Name of streaming service "Netflix"
+                //icon: Image of streaming service
+                //id: identifier of service
+                //name: more specific name "NetflixUS"
+                //url: url of movie/show on service site (location on Netflix site)
+
+        //name: name of movie/tv show "Bojack Horseman"
+        //picture: image of movie/tv show
+        //weight: no idea what this is for
+
+// Content name: data.results[0].name
+// Content picture: data.results[0].picture
+// Content streaming (first location): data.results[0].locations[0].display_name
+// Content url (first location): data.results[0].locations[0].url
+
+/****NOTE*******/
+// If there isn't a picture available for item or service, array/object holds NULL value
+
+
+
+
+
+
 const apiKey = "52122fb71bmsh21d596050a233b7p18b88ajsnf76ae4c3d8e5";
+
+
+
+
+
+
+
+
+// Testing parsing correct data
+const parseData = (results) => {
+    console.log(results[0].name);
+    console.log(results[0].picture);
+    console.log(results[0].locations[0].display_name);
+    console.log(results[0].locations[0].url);
+}
+
+
+
+
+
 
 $(() => {
     $.ajax({
@@ -16,8 +77,8 @@ $(() => {
         type: 'GET',
         dataType: "json",
         data: {
-            // search term
-            term: "bojack",
+            // search term, what user will input
+            term: "bojack horseman",
             // country, either us or uk
             country: "us"
         },
@@ -29,7 +90,8 @@ $(() => {
     }).then((data) => {
         // returning data
         // data = JSON.stringify(data);
-        console.log(data.results);
+        // console.log(data.results);
+        parseData(data.results);
     }, (error) => {
         console.error(error);
     })
