aaed2354aba174761ea983591509677d04a70224
Deva <dkothand@gmail.com>
2019-04-05 11:56:03 -0400

    implemented ajax request over unirest, pass api key into setRequestHeader to have call return 200

--- utelly_api_project/app.js
+++ utelly_api_project/app.js
@@ -1,2 +1,22 @@
-console.log('online');
-console.log($);
+// Solution for using ajax instead of unirest
+// https://mashape.groovehq.com/knowledge_base/categories/consume-an-api/topics
+// Key is to pass API key into setRequestHeader in beforeSend function
+// I think it does the same thing as unirest.().header("API Key here")
+$(() => {
+    $.ajax({
+        url: "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=bojack&country=uk",
+        type: 'GET',
+        dataType: "json",
+        beforeSend: (xhr) => {
+            xhr.setRequestHeader(
+                "X-RapidAPI-Key", "52122fb71bmsh21d596050a233b7p18b88ajsnf76ae4c3d8e5"
+            );
+        }
+    }).then((data) => {
+        // returning data
+        // data = JSON.stringify(data);
+        console.log(data.results);
+    }, (error) => {
+        console.error(error);
+    })
+})
